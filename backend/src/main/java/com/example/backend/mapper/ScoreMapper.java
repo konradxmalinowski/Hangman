c742f93d7d73701b490af049e348fa966a2e06a0
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-12-30 20:05:07 +0100

    Add ScoreMapper

---
+++ backend/src/main/java/com/example/backend/mapper/ScoreMapper.java
@@ -0,0 +1,24 @@
+package com.example.backend.mapper;
+
+import com.example.backend.Score;
+import com.example.backend.dto.ScoreDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScoreMapper {
+
+    private final ModelMapper modelMapper;
+
+    public ScoreMapper() {
+        this.modelMapper = new ModelMapper();
+    }
+
+    public ScoreDTO toDTO(Score score) {
+        return modelMapper.map(score, ScoreDTO.class);
+    }
+
+    public Score toEntity(ScoreDTO scoreDTO) {
+        return modelMapper.map(scoreDTO, Score.class);
+    }
+}
