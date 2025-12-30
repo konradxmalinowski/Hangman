58ba7bd5fa5c9e49e57a9aff910664931a493b0c
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-12-30 20:05:07 +0100

    Add ErrorResponse DTO

---
+++ backend/src/main/java/com/example/backend/dto/ErrorResponse.java
@@ -0,0 +1,31 @@
+package com.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private List<String> details;
+
+    public ErrorResponse(LocalDateTime timestamp, int status, String error, String message, String path) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
+}
