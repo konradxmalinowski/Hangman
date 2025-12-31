ee640c60284392b2067c8a174b60a0bfc15eac2b
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-12-31 12:57:05 +0100

    Add @Serial

--- backend/src/main/java/com/example/backend/dto/ErrorResponse.java
+++ backend/src/main/java/com/example/backend/dto/ErrorResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class ErrorResponse implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
     private LocalDateTime timestamp;
     private int status;
