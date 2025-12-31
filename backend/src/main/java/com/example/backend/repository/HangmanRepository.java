afc82854d667217f027498682637702a38bc1fbc
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-12-31 12:54:17 +0100

    Mv repository

--- backend/src/main/java/com/example/backend/repository/HangmanRepository.java
+++ backend/src/main/java/com/example/backend/repository/HangmanRepository.java
@@ -1,5 +1,6 @@
-package com.example.backend;
+package com.example.backend.repository;
 
+import com.example.backend.entity.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HangmanRepository extends JpaRepository<Score, Integer> {
